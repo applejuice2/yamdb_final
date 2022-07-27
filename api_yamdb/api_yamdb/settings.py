a484e8946751e4d0a447e135828397168e05a66c
Vlad <vlad@MacBook-Air-Vlad.local>
2022-07-27 23:02:41 +0300

    Changed  delpoy job and debug mode on
    Made migrations, collected static and load db after deploy on serve.
    In settings.py added debug=true to watch the problem in django admin panel.

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -6,7 +6,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = os.getenv('SECRET_KEY', default='secretkey')
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
