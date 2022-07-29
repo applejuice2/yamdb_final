9c7daf4f3f8b64cc2cddce1f37383f7c33b4a4cc
Vlad <vlad@MacBook-Air-Vlad.local>
2022-07-29 04:34:04 +0300

    edited mail settings

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -123,7 +123,8 @@ SIMPLE_JWT = {
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.yandex.ru'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+EMAIL_PORT = 465
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
 EMAIL_HOST_USER = os.getenv('MAIL', default=None)
 EMAIL_HOST_PASSWORD = os.getenv('MAIL_PASSWORD', default=None)
